7b4ee863fd5e2930325ebe4e044c51a0eff8c520
Gwers1 <lnathanu@gmail.com>
2023-08-29 15:49:31 +1000

    Copied from react webcam

---
+++ src/pages/admin/monitor.tsx
@@ -0,0 +1,9 @@
+import Camera from "../components/Camera"
+export default function Monitor() {
+    return(
+        <div>
+            <Camera/>
+            <p>Placeholder!</p>
+        </div>
+    )
+}
