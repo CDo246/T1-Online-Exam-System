58014e208f8cc6065bf8e7b43e34e666dfaa2727
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-09-21 15:25:10 +1000

    Added serverside validation, error handling

--- src/pages/api/createaccount.ts
+++ src/pages/api/createaccount.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "~/server/db";
 import { createHash, randomBytes } from "crypto";
+import validator from "validator";
 
 type ResponseData = {
   message: string;
@@ -15,18 +16,31 @@ export default function handler(
   const hash = createHash("sha256").update(`${salt}${password}`).digest("hex");
   console.log(hash);
   //Validate password
-  prisma.user
-    .create({
-      data: {
-        name: name,
-        email: email,
-        password: hash,
-        passwordSalt: salt,
-        role: "Account",
-      },
-    })
-    .then((res) => console.log(res));
-  console.log("TEST");
-  res.status(200).json({ message: "Hello" });
+
   //Error 400 if the account creation fails for some reason
+  if(name === "" || !validator.isEmail(email) || !validator.isStrongPassword(password)) {
+    res.status(400).json({ message: "Incorrect Details" });
+  }
+  else {
+    console.log("Creating account")
+    prisma.user
+      .create({
+        data: {
+          name: name,
+          email: email,
+          password: hash,
+          passwordSalt: salt,
+          role: "Account",
+        },
+      })
+      .then(result => {
+        console.log(result)
+        res.status(200).json({ message: "Account Created." });
+      })
+      .catch(e => {
+        console.log(e)
+        res.status(400).json({ message: "An Error Occured Creating Your Account. There is likely already an account with this email."});
+      })
+  }
 }
+
