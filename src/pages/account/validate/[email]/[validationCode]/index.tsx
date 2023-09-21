eef703381e65ca60b0ec34d21e364d064824cd9f
Lachlan Wright <79518204+LachlanBWWright@users.noreply.github.com>
2023-09-21 23:08:50 +1000

    Fixed installs, added plcaeholder email validation page

---
+++ src/pages/account/validate/[email]/[validationCode]/index.tsx
@@ -0,0 +1,14 @@
+import { FormBox } from "~/components/boxes";
+import { CentredLayout } from "~/components/layouts";
+
+export default function Validation() {
+
+
+    return(
+        <CentredLayout title="Account Validation">
+            <FormBox>
+                <p>TEST</p>
+            </FormBox>
+        </CentredLayout>
+    )
+}
