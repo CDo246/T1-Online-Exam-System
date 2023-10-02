349fe13d37accac0d091e4b9feb7e15552e1e9fd
lachlan.b.wright@student.uts.edu.au <lachlan.b.wright@student.uts.edu.au>
2023-10-03 03:27:21 +1100

    Updated account page

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,26 +1,39 @@
 import Link from "next/link";
-import { BlackBackButton, BlackButton } from "~/components/button";
+import { BlackButton } from "~/components/button";
 import { FormBox } from "~/components/boxes";
 import { CentredLayout } from "~/components/layouts";
 import { getCsrfToken, signOut, useSession } from "next-auth/react";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { redirect } from "next/dist/server/api-utils";
+import router from "next/router"
 
 export default function Account({
   csrfToken,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const { data: session, status } = useSession();
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated() {router.push('/')}
+    }
+  );
+  console.log(session)
   return (
     <CentredLayout title="Create Account">
       <FormBox>
-        <Link href="/">
-          <BlackBackButton />
-        </Link>
-        <hr />
-        <p>Account Stuff</p>
+        <div className="grid grid-flow-col grid-cols-[1fr,1fr,5fr,1fr,1fr] gap-1 overflow-x-auto">
+          <div/>
+          <div/>
+          <p className="text-center text-xl flex-1 align-center">Welcome, {session?.user.name ?? ""}</p>
+          <Link href="/account/settings">
+            <BlackButton text="Account Settings"/>
+          </Link>
+          <a onClick={() => {signOut(); router.push('/');}}>
+            <BlackButton text="Log Out"/>
+          </a>
+        </div>
+        <hr/>
         {session && (
           <div>
-            Signed in as {session.user.email ?? "placeholder"} <br />
-            <button onClick={() => signOut()}>Sign out</button>
+            <p>Signed in as {session.user.email ?? ""}</p> 
           </div>
         )}
       </FormBox>
