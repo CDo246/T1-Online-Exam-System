eee4fcea1b387cb733e7d0a27a8f7a8043a7e3ae
CDo246 <celinedo9999@gmail.com>
2023-10-09 18:26:41 +1100

    Added client popups for email verification

--- src/pages/account/validate/[email]/[verificationCode]/index.tsx
+++ src/pages/account/validate/[email]/[verificationCode]/index.tsx
@@ -9,7 +9,7 @@ export default function Validation() {
   const router = useRouter();
 
   const verifyAccount = api.accounts.verifyAccount.useMutation();
-
+  const disabled = verifyAccount.isLoading || verifyAccount.isSuccess
   return (
     <CentredLayout title="Account Validation">
       <FormBox>
@@ -17,13 +17,33 @@ export default function Validation() {
         <p>Click the button to validate your account:</p>
         <a
           onClick={async () => {
-            await verifyAccount.mutateAsync({email : router.query.email as string, 
-              verificationCode: router.query.verificationCode as string
+            if (disabled) {
+              return;
+            }
+            await verifyAccount.mutateAsync({
+              email: router.query.email as string,
+              verificationCode: router.query.verificationCode as string,
             });
           }}
         >
-          <BlackButton text="Verify Account" />
+          <BlackButton text="Verify Account" disabled = {disabled}/>
         </a>
+        {verifyAccount.error && (
+          <div className="grid grid-cols-[1fr_auto_1fr] rounded-full bg-red-700">
+            <div />
+            <p className="text-white">
+              {verifyAccount.error.message}
+            </p>
+          </div>
+        )}
+        {verifyAccount.isSuccess && (
+          <div className="grid grid-cols-[1fr_auto_1fr] rounded-full bg-green-800">
+            <div />
+            <p className="text-white">
+              Email Validation Success 👍
+            </p>
+          </div>
+        )}
       </FormBox>
     </CentredLayout>
   );
