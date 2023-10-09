48831816912768f911c40fea5e860211efaf5566
CDo246 <celinedo9999@gmail.com>
2023-10-09 18:15:28 +1100

    Changed client for verifyAccount to use trpc

--- src/pages/account/validate/[email]/[verificationCode]/index.tsx
+++ src/pages/account/validate/[email]/[verificationCode]/index.tsx
@@ -1,10 +1,14 @@
+import Email from "next-auth/providers/email";
 import { useRouter } from "next/router";
 import { FormBox } from "~/components/boxes";
 import { BlackButton } from "~/components/button";
 import { CentredLayout } from "~/components/layouts";
+import { api } from "~/utils/api";
 
 export default function Validation() {
   const router = useRouter();
+
+  const verifyAccount = api.accounts.verifyAccount.useMutation();
 
   return (
     <CentredLayout title="Account Validation">
@@ -12,17 +16,10 @@ export default function Validation() {
         <p>Your email address is: {router.query.email}</p>
         <p>Click the button to validate your account:</p>
         <a
-          onClick={() => {
-            fetch("/api/verifyaccount", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                email: router.query.email,
-                verificationCode: router.query.verificationCode,
-              }),
-            }).then((res) => console.log(res));
+          onClick={async () => {
+            await verifyAccount.mutateAsync({email : router.query.email as string, 
+              verificationCode: router.query.verificationCode as string
+            });
           }}
         >
           <BlackButton text="Verify Account" />
