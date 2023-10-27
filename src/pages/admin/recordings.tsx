fc2ad5a21b451593bb42053ce72581ba29d80d90
Kai Sor <kai.p.sor@student.uts.edu.au>
2023-10-28 00:06:41 +1100

    Moved listing query to server

--- src/pages/admin/recordings.tsx
+++ src/pages/admin/recordings.tsx
@@ -4,27 +4,31 @@ import { BlackBackButton, BlackButton } from "~/components/button";
 import { CentredLayout } from "~/components/layouts";
 import { useState } from "react";
 import AWS from "aws-sdk";
+import { api } from "~/utils/api";
 
 export default function Recordings() {
     //function that retrieves all 0bjects in s3 bucket
     const [objects, setObjects] = useState<string []>([]);
+    const {data} = api.externalAPIs.listRecordings.useQuery();
 
-    const config = {
-      accessKeyId: '',
-      secretAccessKey: '',
-    region: 'ap-southeast-2',
-    }
+    // const config = {
+    //   accessKeyId: '',
+    //   secretAccessKey: '',
+    // region: 'ap-southeast-2',
+    // }
 
-      AWS.config.update(config);
-      const client = new AWS.S3({ params: { Bucket: "online-anti-cheat" } });
+    //   AWS.config.update(config);
+    //   const client = new AWS.S3({ params: { Bucket: "online-anti-cheat" } });
 
       //list all files
       const listS3Objects = async () => {
         try {
-          const response = await client.listObjectsV2().promise();
-          const objects = (response.Contents || []) as { Key: string }[];
-          const s3Objects = objects.map((object) => object.Key);
-          setObjects(s3Objects);
+          
+          if(data){
+            const s3Objects = data.s3Objects;
+            setObjects(s3Objects);
+          }
+          
         } catch (error) {
           console.error("Error listing S3 objects:", error);
         }
@@ -53,8 +57,6 @@ export default function Recordings() {
           </li>
         ))}
       </ul>
-    
-    <p>TESTING</p>
     </div>
     
   );
