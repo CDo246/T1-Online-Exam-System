6a8526ccb0fb5923d9f185d3f09b412324825b96
Kai Sor <kai.p.sor@student.uts.edu.au>
2023-10-28 00:41:23 +1100

    prettier fix

--- src/pages/admin/recordings.tsx
+++ src/pages/admin/recordings.tsx
@@ -7,56 +7,54 @@ import AWS from "aws-sdk";
 import { api } from "~/utils/api";
 
 export default function Recordings() {
-    //function that retrieves all 0bjects in s3 bucket
-    const [objects, setObjects] = useState<string []>([]);
-    const {data} = api.externalAPIs.listRecordings.useQuery();
+  //function that retrieves all 0bjects in s3 bucket
+  const [objects, setObjects] = useState<string[]>([]);
+  const { data } = api.externalAPIs.listRecordings.useQuery();
 
-    // const config = {
-    //   accessKeyId: '',
-    //   secretAccessKey: '',
-    // region: 'ap-southeast-2',
-    // }
+  // const config = {
+  //   accessKeyId: '',
+  //   secretAccessKey: '',
+  // region: 'ap-southeast-2',
+  // }
 
-    //   AWS.config.update(config);
-    //   const client = new AWS.S3({ params: { Bucket: "online-anti-cheat" } });
+  //   AWS.config.update(config);
+  //   const client = new AWS.S3({ params: { Bucket: "online-anti-cheat" } });
 
-      //list all files
-      const listS3Objects = () => {
-        try {
-          if(data){
-            const s3Objects = data.s3Objects;
-            setObjects(s3Objects);
-          }
-          
-        } catch (error) {
-          console.error("Error listing S3 objects:", error);
-        }
-      };
+  //list all files
+  const listS3Objects = () => {
+    try {
+      if (data) {
+        const s3Objects = data.s3Objects;
+        setObjects(s3Objects);
+      }
+    } catch (error) {
+      console.error("Error listing S3 objects:", error);
+    }
+  };
 
-      //download file with objectKey
-      const downloadFile = (objectKey : string) => {
-        const link = document.createElement('a');
-        const bucketName = "online-anti-cheat";
-        link.href = `https://${bucketName}.s3.amazonaws.com/${objectKey}`;
-        link.target = '_blank';
-        link.download = objectKey; // Set the suggested file name
-        link.click();
-      };
+  //download file with objectKey
+  const downloadFile = (objectKey: string) => {
+    const link = document.createElement("a");
+    const bucketName = "online-anti-cheat";
+    link.href = `https://${bucketName}.s3.amazonaws.com/${objectKey}`;
+    link.target = "_blank";
+    link.download = objectKey; // Set the suggested file name
+    link.click();
+  };
 
   return (
     <div>
-        <a onClick={listS3Objects}>
-            <BlackButton text="refresh" />
-          </a>
+      <a onClick={listS3Objects}>
+        <BlackButton text="refresh" />
+      </a>
       <h1>Exam Session Video Recordings</h1>
       <ul>
         {objects.map((objectKey, index) => (
           <li key={index}>
-            <a onClick={()=>downloadFile(objectKey)}>{objectKey}</a>
+            <a onClick={() => downloadFile(objectKey)}>{objectKey}</a>
           </li>
         ))}
       </ul>
     </div>
-    
   );
 }
--- src/server/api/routers/externalapi.ts
+++ src/server/api/routers/externalapi.ts
@@ -125,24 +125,21 @@ export const externalAPIRouter = createTRPCRouter({
       };
     }),
 
+  listRecordings: publicProcedure.query(async ({ input, ctx }) => {
+    const config = {
+      accessKeyId: `${process.env.AMAZON_ACCESS_KEY_ID}`,
+      secretAccessKey: `${process.env.AMAZON_SECRET_ACCESS_KEY}`,
+      region: "ap-southeast-2",
+    };
 
+    AWS.config.update(config);
+    const client = new AWS.S3({ params: { Bucket: "online-anti-cheat" } });
 
-  listRecordings : publicProcedure
-    .query(async ({input, ctx}) => {
-      const config = {
-        accessKeyId: `${process.env.AMAZON_ACCESS_KEY_ID}`,
-        secretAccessKey: `${process.env.AMAZON_SECRET_ACCESS_KEY}`,
-        region: 'ap-southeast-2',
-      };
-      
-      AWS.config.update(config);
-      const client = new AWS.S3({ params: { Bucket: "online-anti-cheat" } });
-
-      const response = await client.listObjectsV2().promise();
-          const objects = (response.Contents ?? []) as { Key: string }[];
-          const s3Objects = objects.map((object) => object.Key);
-      return {
-        s3Objects: s3Objects,
-      }
-    }),
+    const response = await client.listObjectsV2().promise();
+    const objects = (response.Contents ?? []) as { Key: string }[];
+    const s3Objects = objects.map((object) => object.Key);
+    return {
+      s3Objects: s3Objects,
+    };
+  }),
 });
